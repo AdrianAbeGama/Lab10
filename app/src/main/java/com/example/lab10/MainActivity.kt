77560282d480a6b2586d355e71458fadc8cd7324
AdrianGamarra <adrian.gamarra@tecsup.edu.pe>
2024-10-13 19:08:52 -0500

    PRIMER COMMIT

---
+++ app/src/main/java/com/example/lab10/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.lab10
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lab10.ui.theme.Lab10Theme
+
+
+import androidx.activity.compose.setContent
+import view.SeriesApp // Asegúrate de que la ruta del paquete sea correcta
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Lab10Theme {
+                SeriesApp()  // Aquí llamas a la función SeriesApp en lugar de Greeting
+            }
+       }
+      }
+}
